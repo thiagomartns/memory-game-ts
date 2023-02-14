7ec01dd1c7a77a3e09c4df8906dbf6206e98651c
thiago <tmg3.mn@gmail.com>
2023-02-14 11:24:29 -0300

    Função 'handleClick' adicionada a coverImg

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ interface CardImage {
   id: number;
 }
 
+interface CardChoice {
+  src: string,
+  id: number
+}
+
 const cardImages: CardImage[] = [
   { "src": "assets/img/helmet-1.png", id: 1 },
   { "src": "assets/img/potion-1.png", id: 2 },
@@ -20,6 +25,8 @@ function App() {
 
   const [cards, setCards] = useState<CardImage[]>([])
   const [turns, setTurns] = useState<number>(0)
+  const [choiceOne, setChoiceOne] = useState<CardChoice | null>(null)
+  const [choiceTwo, setChoiceTwo] = useState<CardChoice | null>(null)
 
   const shuffleCards = (): void => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -29,6 +36,10 @@ function App() {
     setCards(shuffledCards)
     setTurns(0);
   }
+
+  const handleChoice = (card: CardChoice) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  }
   
   return (
     <div className="App">
@@ -37,7 +48,7 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard card={card} />
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
--- src/components/singleCard/SingleCard.tsx
+++ src/components/singleCard/SingleCard.tsx
@@ -6,15 +6,21 @@ interface CardProps {
   card: {
     src: string,
     id: number
-  }
+  },
+  handleChoice: (card: {src: string, id: number}) => void;
 }
 
-const SingleCard = ({ card }: CardProps) => {
+const SingleCard = ({ card, handleChoice }: CardProps) => {
+
+  const handleClick = () => {
+    handleChoice(card)
+  }
+
   return (
     <div className="card" key={card.id}>
       <div>
         <img className="front" src={card.src} alt="card front" />
-        <img className="back" src={coverImg} alt="cover" />
+        <img className="back" src={coverImg} alt="cover" onClick={handleClick} />
       </div>
     </div>
   )
